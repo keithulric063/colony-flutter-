0879aa1845b340ff3a4c292e3511c68644e0991c
keithulric063 <keithulric063@gmail.com>
2026-08-11 12:18:46 +0200

    feat collection

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,61 @@
-int age = 18;
-String sexe = "Masculin";
-main() {
-  //structure alternative
-  if (age < 18) {
-    print("Mineur");
-  } else if (age == 18) {
-    print("Enfant difficile");
-  } else {
-    print("Majeur");
-  }
+import 'dart:nativewrappers/_internal/vm/bin/vmservice_io.dart';
 
-  //structure imbriquée
-  if (age < 18) {
-    if (sexe == "Masculin") {
-      print("Mineur");
-    } else if (sexe == "féminin") {
-      print("Mineure");
-    }
-  } else if (age == 18) {
-    if (sexe == "Masculin") {
-      print("Mineur difficile");
-    } else if (sexe == "féminin") {
-      print("Mineure difficile");
-    }
-  } else {
-    if (sexe == "Masculin") {
-      print("Majeur");
-    } else {
-      print("Majeure");
+main() {
+  //     int age = 18;
+  //     String sexe = "féminin";
+  //   //structure alternative
+  //   if (age < 18) {
+  //     print("Mineur");
+  //   } else if (age == 18) {
+  //     print("Enfant difficile");
+  //   } else {
+  //     print("Majeur");
+  //   }
+
+  //   //structure imbriquée
+  //   if (age < 18) {
+  //     if (sexe == "Masculin") {
+  //       print("Mineur");
+  //     } else if (sexe == "féminin") {
+  //       print("Mineure");
+  //     }
+  //   } else if (age == 18) {
+  //     if (sexe == "Masculin") {
+  //       print("Mineur difficile");
+  //     } else if (sexe == "féminin") {
+  //       print("Mineure difficile");
+  //     }
+  //   } else {
+  //     if (sexe == "Masculin") {
+  //       print("Majeur");
+  //     } else {
+  //       print("Majeure");
+  //     }
+  //   } // structure ternaire
+  //   print(
+  //     (age < 18)
+  //         ? ((sexe == "Masculin") ? "homme Mineur" : "femme mineure")
+  //         : ((sexe == "Masculin") ? "homme Majeur" : "femme Majeure"),
+  //   );
+  bool x = false;
+  List<int> ages = [13, 20, 24, 10, 2];
+  List<String> noms = ["ulric", "lucia", "chadrac"];
+  print(ages[3]);
+  ages.add(56);
+  print(ages[5]);
+  print(ages.length);
+  ages.removeAt(5);
+  print(ages.length);
+  ages.remove(10);
+  print(ages[3]);
+  print(noms.contains("chadrac"));
+  for (var nom in noms) {
+    if (nom.toUpperCase() == "chadrac".toUpperCase()) {
+      x = true;
     }
   }
-  print(
-    (age < 18)
-        ? ((sexe == "Masculin") ? "homme Mineur" : "femme mineur")
-        : ((sexe == "Masculin") ? "homme Majeur" : "femme Majeure"),
-  );
+  print(x);
 }
+//liste 
+
+//collection 
